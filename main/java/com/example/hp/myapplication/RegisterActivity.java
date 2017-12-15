18d2f435f1f4a32c63cd733f88dfbc1bd60df838
whlieyoucome <33180447+whlieyoucome@users.noreply.github.com>
2017-12-15 12:13:38 +0800

    homework-第五次-lv2-罗子曦-2017210828
    0.0

---
+++ main/java/com/example/hp/myapplication/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.hp.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Button buttonback = (Button) findViewById(R.id.button_back);
+        buttonback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
